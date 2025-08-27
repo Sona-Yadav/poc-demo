f4bac12e03c0b561e6ba1782400434e9d5c3a987
Sona <127790511+Sona-Yadav@users.noreply.github.com>
2025-08-27 23:57:39 +0530

    Update validate_service.sh

--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
-set -e
-curl --fail http://localhost/ || (echo "service validation failed" && exit 1)
-echo "service validation passed"
+# Wait and check if service is running on port 80
+for i in {1..10}
+do
+  if curl -s http://localhost:80 > /dev/null
+  then
+    echo "Service is running."
+    exit 0
+  else
+    echo "Service not up yet. Retrying in 5s..."
+    sleep 5
+  fi
+done
+
+echo "Service validation failed after retries."
+exit 1
